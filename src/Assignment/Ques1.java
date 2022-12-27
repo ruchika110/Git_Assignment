fed780443207f4ab79bba50f072f55d0637295ad
Ruchika Verma <ruchika.verma@nagarro.com>
2022-12-27 15:48:01 +0530

    my first commit

---
+++ src/Assignment/Ques1.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+public class Ques1 {
+	
+	public static void main(String[] args) {
+
+		String str ="This   is    a   practice     question";
+		String noSpaceStr = str.replaceAll("\\s+"," ");  
+		System.out.println(noSpaceStr); 
+
+	}
+
+}
